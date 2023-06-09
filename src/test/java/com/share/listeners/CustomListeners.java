a32edd67be5c6fc4e1859dc4109f6d88d57b9eb4
DotEsparto <bhautikradadiya6@gmail.com>
2023-06-09 16:37:21 +0530

    skipped staging area fixed

--- src/test/java/com/share/listeners/CustomListeners.java
+++ src/test/java/com/share/listeners/CustomListeners.java
@@ -22,6 +22,7 @@ public class CustomListeners implements ITestListener{
 		//helo
 		Reporter.log("Capturingasf SS");
 		Reporter.log("Capturingasf SS");
+		Reporter.log("Capturingasf SS");
 
 	}
 	
