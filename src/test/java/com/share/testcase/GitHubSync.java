649ef92e475ba3f43f2c001fac35259644782dde
unknown <bhautikradadiya6@gmail.com>
2023-07-16 12:43:01 +0530

    asdf

--- src/test/java/com/share/testcase/GitHubSync.java
+++ src/test/java/com/share/testcase/GitHubSync.java
@@ -1,5 +1,8 @@
 package com.share.testcase;
 
+import java.io.IOException;
+
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import com.share.base.TestBase;
@@ -10,8 +13,15 @@ public class GitHubSync extends TestBase {
 	
 
 	@Test(dataProviderClass = TestUtil.class, dataProvider = "db")
-	public void gitInstall() {
+	public void gitHubSync(String Runmode) throws InterruptedException, IOException{
 
+
+		if (!Runmode.equals("Y")) {
+			
+			throw new SkipException("Skipping the test case as the Run mode for data set is NO");
+		}
+		
+		
 		click("signupLogin_XPATH");
 		click("countinuewithDummyAuth_XPATH");
 		type("countinuewithDummyAuth_XPATH","bhautik-vase");
