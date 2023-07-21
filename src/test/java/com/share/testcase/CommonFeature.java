3095472afff29f9af6c61794bf54afe3c712988a
codem07 <75160826+codem07@users.noreply.github.com>
2023-07-21 14:49:56 +0530

    Stack share (#14)
    * GitHub Sync done
    
    * pom updated
    
    * if loop added on typing org URL on company profile

--- src/test/java/com/share/testcase/CommonFeature.java
+++ src/test/java/com/share/testcase/CommonFeature.java
@@ -174,6 +174,9 @@ public class CommonFeature extends TestBase {
 
 	}
 
+
+	@SuppressWarnings("unlikely-arg-type")
+
 	public void gitHubSync(String orgURL, String companyname) throws IOException {
 
 	
@@ -195,10 +198,19 @@ public class CommonFeature extends TestBase {
 		verifyEquals(strCompanyNameText,"ss-test-" + companyname);
 				
 	    click("install-GitHub-btn_CSS");
-	    
-	    type("org-URL-inputbox_XPATH",orgURL);
 
-	    click("continue-btn_CSS");
+	   
+	    WebElement elementContinueBtn = driver.findElement(By.cssSelector(OR.getProperty("continue-btn_CSS")));
+		String strContinueBtnClass = elementContinueBtn.getAttribute("class");
+	    
+	    if(strContinueBtnClass.equals("css-vy727i")) {
+		    click("continue-btn_CSS");
+	    	
+	    }else {
+	    	type("org-URL-inputbox_XPATH",orgURL);
+	    	 click("continue-btn_CSS");
+	    }
+
 	    
 	    click("next-btn_CSS");
 	    click("next-btn_CSS");
