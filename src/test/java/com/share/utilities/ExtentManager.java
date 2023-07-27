8926883c017829c68ef78c747816a9e794b644d2
DotEsparto <bhautikradadiya6@gmail.com>
2023-07-27 10:56:16 +0530

    asdf

--- src/test/java/com/share/utilities/ExtentManager.java
+++ src/test/java/com/share/utilities/ExtentManager.java
@@ -1,32 +1,44 @@
 package com.share.utilities;
 
 import java.io.File;
+import java.io.IOException;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
 
 
-
+//class 
 public class ExtentManager {
 	
 	private static ExtentReports extent;
 	
-	public static ExtentReports getInstance() {
+	public static ExtentReports getInstance()      {
+		System.out.println("Extent is before the : "+extent);
 		if(extent==null) {
 			ExtentSparkReporter spark = new ExtentSparkReporter("index.html");
-			final File config = new File("");
+			final File config = new File("ReportsConfig.xml");
+			int i = 1;
 			
+		extent = new ExtentReports();
 		
-	/*	spark.config().setTheme(Theme.DARK);
+	/*spark.config().setTheme(Theme.DARK);
 		spark.config().setDocumentTitle("StackShare Automation Report");
 		spark.config().setReportName("Extent Report");
-		*/extent.attachReporter(spark);
+		*/
+			System.out.println("Extent is inside the if : "+extent+i);
+			extent.attachReporter(spark);
 		
+			
 		
-		
+				try {
+					spark.loadXMLConfig(config);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
 		}
-		return extent;
+	return extent;
 	}
 	
 
